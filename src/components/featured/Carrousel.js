e44f905562ae7c0ee6424a1dd21b0d6c69061b15
unknown <catalin.binzari99@gmail.com>
2020-07-15 15:02:21 +0300

    slider section

---
+++ src/components/featured/Carrousel.js
@@ -0,0 +1,57 @@
+//react stateless component
+import React from "react";
+import Slider from "react-slick";
+
+import slide_one from "../../resources/images/slide_one.jpg";
+import slide_two from "../../resources/images/slide_two.jpg";
+import slide_three from "../../resources/images/slide_three.jpg";
+const Carrousel = () => {
+  const settings = {
+    dota: false, //dots are points at the down of the slide
+    infinite: true, //loops in infinite way
+    autoplay: true,
+    speed: 500,
+  };
+  return (
+    <div
+      className="carrousel_wrapper"
+      style={{
+        background: "red",
+        height: `${window.innerHeight}px`,
+        overflow: "hidden",
+      }}
+    >
+      <Slider {...settings}>
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${slide_one})`,
+              height: `${window.innerHeight}px`,
+            }}
+          ></div>
+        </div>
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${slide_two})`,
+              height: `${window.innerHeight}px`,
+            }}
+          ></div>
+        </div>
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${slide_three})`,
+              height: `${window.innerHeight}px`,
+            }}
+          ></div>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Carrousel;
