9c30f6e5eacee4f2c2979455841033004ca4c8b5
unknown <catalin.binzari99@gmail.com>
2020-07-15 23:49:30 +0300

    highlight component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./resources/styles.css";
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured";
 import VenueNfo from "./components/venueNfo";
+import Highlight from "./components/Highlights";
 class App extends Component {
   render() {
     return (
@@ -14,6 +15,7 @@ class App extends Component {
         <Header />
         <Featured />
         <VenueNfo />
+        <Highlight />
       </div>
     );
   }
